7c58bc1a9e9a121ecef1535988ea98726b2b016b
stephen <stevie.barbera@gmail.com>
2018-02-07 18:13:56 -0600

    Initial commit

---
+++ Quiz4.1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int num1 = 1;
+
+	while (num1 < 2001) 
+	{
+		if ((num1 % 50 == 0) && (num1 % 33 == 0)) 
+		{
+			cout << num1 << "FOOBAR" << endl;
+			++num1;
+		}
+		else if (num1 % 50 == 0)
+		{
+			cout << num1 << "FOO" << endl;
+			++num1;
+		}
+		else if (num1 % 33 == 0)
+		{
+			cout << num1 << "BAR" << endl;
+			++num1;
+
+		}
+		else
+		{
+			cout << num1 << endl;
+			++num1;
+		}
+
+
+	}
+
+	return 0;
+
+}
